eaff865841269481f3cb47d8b1b6802c0611fd1d
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-27 23:26:27 +0300

    fixed cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_tK06ufFFluRVey2AUGuoILqGOkBmyoZqONcJSoW1VsReyMDs92CF0FaFvvpO6M7S";
 
+export function fetchBreeds() {
+    return axios.get('https://api.thecatapi.com/v1/breeds').then((response) => {
+        if (!response.data) {
+             new Error(response.status);
+        }
+        else {
+             return response.data;
+        }
+   })
+}
+
+
+
 export function fetchCatByBreed(breedId) {
     let params = new URLSearchParams({
         breed_ids: breedId,
@@ -13,24 +26,6 @@ export function fetchCatByBreed(breedId) {
              return response.data[0];
         }
    })
-}
-
-
-
-export function fetchCatByBreed(breedId) {
-    return axios.get('https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}')
-    .then(response => response.data[0])
-    // .then(response => {
-    //     if(!response.data) {
-    //         new Error(response.status);
-    //     }
-    // else {
-    //     return response.data;
-    // }
-    // })
-    .catch(error => {
-        throw error; 
-    });
 }
 
 
